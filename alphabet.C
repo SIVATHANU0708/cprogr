b57d4f1292816b5333805e368d9922fb4ca222f1
SIVATHANU0708 <35222125+SIVATHANU0708@users.noreply.github.com>
2018-01-22 16:27:24 +0530

    alphabet.C
    alphabet or not

---
+++ alphabet.C
@@ -0,0 +1,16 @@
+#include<stdio.h>
+int main()
+{
+char ch;
+printf("enter the charcter:");
+scanf(%c",&ch);
+if((ch=='a' && ch<=='z')||(ch=='A' && ch<=='Z'))
+{
+printf("it is an alphabet");
+}
+else
+{
+printf("it is not an alphabet");
+}
+return 0;
+}
